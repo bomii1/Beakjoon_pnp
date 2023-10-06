f10376ddfe98f6eb00a26d9e3b343a7ab300fcec
bomii1 <buij304@naver.com>
2023-10-06 22:48:56 +0900

    1260

--- 1260.py
+++ 1260.py
@@ -4,7 +4,7 @@ def DFS(V):
     visitedD[V] = 1
     print(V, end=' ')
     for i in range(1, N+1):
-        if graph[V][i] == 1 and visitedD[i] == 0:
+        if matrix[V][i] == 1 and visitedD[i] == 0:
             DFS(i)
 
 def BFS(V):
@@ -17,20 +17,20 @@ def BFS(V):
         print(node, end=' ')
 
         for i in range(1, N+1):
-            if graph[node][i] == 1 and visitedB[i] == 0:
+            if matrix[node][i] == 1 and visitedB[i] == 0:
                 queue.append(i)
                 visitedB[i] = 1
 
 N, M, V = map(int, input().split())
 
-graph = [[0 for j in range(N+1)] for i in range(N+1)]
+matrix = [[0 for j in range(N+1)] for i in range(N+1)]
 visitedD = [0] * (N+1)
 visitedB = [0] * (N+1)
 
 for i in range(M):
     a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+    matrix[a][b] = 1
+    matrix[b][a] = 1
 
 DFS(V)
 print()
